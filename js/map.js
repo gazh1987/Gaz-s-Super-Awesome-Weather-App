e092668847c35459ff88516cadfe81006694d8e7
Gary Healy <gary.healy.87@gmail.com>
2023-12-12 21:11:06 +0000

    Added layers to check the weather

--- js/map.js
+++ js/map.js
@@ -1,18 +1,26 @@
 var map = null;
 
 var Map = function() {
-    map = L.map('map', { zoomControl:false, attributionControl:false }).fitWorld(); 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-    }).addTo(map);
-    map.locate({setView: true, maxZoom: 14});
+    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    maxZoom: 10, attribution: { zoomControl:false, attributionControl:false } });
+
+    var cloudscls = L.OWM.cloudsClassic({appId: owmApiKey});
+    var rain = L.OWM.rain({appId: owmApiKey});
+    var temp = L.OWM.temperature({appId: owmApiKey});
+    var wind = L.OWM.wind({appId: owmApiKey});
+
+    var map = L.map('map', { layers: [osm] });
+    var baseMaps = { "Layers": osm };
+    var overlayMaps = { "Clouds": cloudscls, "Rain": rain, "Temperature": temp, "Wind": wind };
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+    
+    map.locate({setView: true, maxZoom: 10});
 
     function onLocationFound(e) {
         L.marker(e.latlng).addTo(map);
-
         var lat = e.latlng.lat;
         var lon = e.latlng.lng;
-
+        
         // Make a GET request to the OpenWeatherMap API
         // apiKey comes from config.js file
         const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${owmApiKey}`;
@@ -45,6 +53,7 @@ var Map = function() {
             .catch(error => {
                 console.error('Error fetching data from OpenWeatherMap API:', error);
         });
+
     }
 
     function onLocationError(e) {
